02055e04b4fb69036b97798a36f855c2aa72d089
Christopher Thiebaut <christopherthiebaut@gmail.com>
2019-03-22 15:05:21 -0400

    Initial Commit

---
+++ GitHub Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Browser
+//
+//  Created by Christopher Thiebaut on 3/22/19.
+//  Copyright © 2019 Christopher Thiebaut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GitHub BrowserTests/GitHub_BrowserTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHub_BrowserTests.swift
+//  GitHub BrowserTests
+//
+//  Created by Christopher Thiebaut on 3/22/19.
+//  Copyright © 2019 Christopher Thiebaut. All rights reserved.
+//
+
+import XCTest
+@testable import GitHub_Browser
+
+class GitHub_BrowserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
